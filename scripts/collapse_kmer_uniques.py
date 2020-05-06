d4ede16cc60d23836937cbb4d98d4bcb9df9f048
Rachael M. Cox <42787920+rachaelcox@users.noreply.github.com>
2020-05-06 14:17:02 -0500

    Add files via upload

--- scripts/collapse_kmer_uniques.py
+++ scripts/collapse_kmer_uniques.py
@@ -4,20 +4,25 @@ import argparse
 from Bio import SeqIO
 from itertools import groupby
 
- # generate unique edge list with aggregate protein IDs
+
+# generate unique edge list with aggregate protein IDs
 
 def unique(input_file):
     
+    # read input to dataframe & create new file name
     output_file = input_file.replace(".csv","")
     n_df = pd.read_csv(input_file)
     n_df.columns = ['Node1','Node2','ProteinID']    
-
     kmer_df = n_df[['Node1','Node2','ProteinID']]   
-    #print(kmer_df)               
+    
+    # unique the kmer edges & aggregate the protein IDs in column 3             
     uniques_df = kmer_df.groupby(['Node1','Node2'],sort=False).agg(lambda x: set(x)).reset_index()
-
+    
+    # change the protein ID aggregate lists to a less annoying format
+    # and also create a protein count column
     count_list = []
     prot_list = []
+
     for entry in uniques_df['ProteinID']:
         count_list.append(len(entry))
         prot_list.append(list(entry))
@@ -25,9 +30,11 @@ def unique(input_file):
     uniques_df['ProteinID'] = prot_list
     uniques_df.insert(3, 'ProteinCount', count_list)      
 
-    uniques_df.to_csv("{}_uniques.csv".format(output_file),index=False)
+    # write the output to a new file
+    uniques_df = uniques_df.sort_values('ProteinCount', ascending=False)
+    uniques_df.to_csv("{}_unique.csv".format(output_file),index=False)
         
-    print(uniques_df)
+    #print(uniques_df)
 
 if __name__ == "__main__":
     
--- scripts/generate_deBruijn_kmers.py
+++ scripts/generate_deBruijn_kmers.py
@@ -1,41 +1,39 @@
 import csv
-import pandas as pd
 import re
 import argparse
 from Bio import SeqIO
-from itertools import groupby
 
 # in a de Bruijn graph, edges=kmers and nodes=(k-1)mers
 # specify FASTA file, desired kmer and number of proteins
-def edgelist(input_fasta,kmer_size,num_prot):
+def edgelist(input_fasta,kmer_size):
     
     # define new file name, create empty list(s) and data frame(s)
     writefile = input_fasta.replace(".fasta",'')
     nodes = []
-    n_df = pd.DataFrame()
     
     # extract lists of nodes and edges from FASTA file, then write edge list to csv
-    with open("{}_nodes_{}mer_{}proteins.csv".format(writefile,kmer_size,num_prot),"w") as f:
-        for index,record in enumerate((SeqIO.parse(open(input_fasta,"r"), "fasta"))):
-            index += 1
+    with open("{}_dBg_{}mers.csv".format(writefile,kmer_size),"w") as f:
+        
+        f.write("%s,%s,%s\n"%("Node1","Node2","ProteinID"))
+        for record in SeqIO.parse(open(input_fasta,"r"), "fasta"):
+            
             seq_str = record.seq
-            ProteinID = record.id
-            f.write("%s,%s,%s\n"%("Node1","Node2","ProteinID"))
+
+            pattern = r'(tr|sp)\|(.*)\|\S*'
+            match = re.search(pattern, str(record.id))
+            if match:
+                ProteinID = match.group(2)
+            else:
+                ProteinID = record.id
+
             for i in range(len(seq_str)):
                 n1 = seq_str[i:i+(kmer_size-1)]
                 n2 = seq_str[i+1:i+kmer_size]
-                if len(n1) and len(n2) == kmer_size-1:
-                    nodes.append([str(n1),str(n2),ProteinID])
+                
+                if len(n1) and len(n2) == kmer_size - 1:
                     f.write("%s,%s,%s\n"%(n1,n2,ProteinID))
-            if index == num_prot:
-                break
-    
-    # generate unique edge list with aggregate protein IDs
-    #n_df = pd.DataFrame(nodes)
-    #n_df.columns = ['Node1','Node2','ProteinID']
-    #uniques = n_df.groupby(['Node1','Node2'],sort=False).agg(lambda x: set(x)).reset_index()
-    #uniques.to_csv("{}_nodes_{}mer_{}prots_uniques.csv".format(writefile,kmer_size,num_prot))
-    #print(uniques)
+                    print("{} edge #{} = {}-{}".format(ProteinID,i,n1,n2))
+            
 
 if __name__ == "__main__":
     
@@ -43,12 +41,6 @@ if __name__ == "__main__":
     parser.add_argument("--input_fasta", action="store", required=True,
                                         help="Filename of .fasta input")
     parser.add_argument("--kmer_size", action="store", type=int, required=True,
-                                        help="Size of kmer edges")
-    parser.add_argument("--num_prot", action="store", required=False, default='all',
-                                        help="Number of proteins to process; default=all")
-    parser.add_argument("--sep", action="store", required=False, default=',',
-                                        help="Column separator for input file, default=,")
-    
+                                        help="Size of kmer edges") 
     inputs = parser.parse_args()
-    print(inputs)
-    kmer_file = edgelist(inputs.input_fasta, inputs.kmer_size, inputs.num_prot)
+    kmer_file = edgelist(inputs.input_fasta, inputs.kmer_size)
